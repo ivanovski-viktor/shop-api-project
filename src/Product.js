eb263d675dcd438e41e1ae3932294818a8cf0b3b
vikmeisterr <viktor.ivanovski11@gmail.com>
2024-02-05 22:19:41 +0100

    Created main layout

--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,25 @@
 import "./styles/products.scss";
-import Products from "./products.js";
+import Products from "./Products.js";
 
 export default async function Product() {
   const productsArray = await Products();
+  function clickHandler(e) {
+    console.log(e.target);
+    e.target.classList.toggle("clicked");
+  }
   //Truncatng product title
   for (const product of productsArray) {
     product.title.length > 40
       ? (product.title = product.title.substr(0, 39) + "...")
-      : (product.title = product.title);
+      : product.title;
   }
-  console.log(productsArray);
+
   const container = document.createElement("div");
   productsArray.forEach(({ title, image, price, description }) => {
     const element = document.createElement("div");
 
     element.innerHTML = `<div class="productCard">
-        <h5>${title}</h5>"
+        <h5>${title}</h5>
         <img src="${image}">
         <span class="priceContainer">
         ${
@@ -26,10 +30,14 @@ export default async function Product() {
             : `<span>$${price}</span>`
         }
         </span>
-        <button onclick="this.classList.add('clicked')">Add To Cart</button>
+        <button class="addToCartButton">Add To Cart</button>
   </div>`;
     container.classList.add("productContainer");
     container.appendChild(element);
+    const addToCartButtons = element.querySelectorAll(".addToCartButton");
+    addToCartButtons.forEach((button) => {
+      button.addEventListener("click", clickHandler);
+    });
   });
   return container;
 }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import "./styles/main.scss";
-import Product from "./product.js";
+import "./styles/index.scss";
+import Product from "./Product.js";
 
-document.body.appendChild(await Product());
-// function main() {
-//   const productsArray = Products();
-//   const element = document.createElement("div");
-//   //Lodash imported
-//   element.innerHTML = `<div class="firstClass">
-//   </div>`;
+function root() {
+  return ` <div class="root">
+            <header class="shopHeader">
+              <img class="shopLogo" src="" alt="Shop Logo">
+              <nav></nav>
+            </header>
+            <main class="shopMain"> 
+              <aside class="sideBar"><select><option value='all'>All Categories</option></select></aside>
+              <div class="productsHolder"></div>
+            </main>
+            </div>`;
+}
 
-//   return element;
-// }
-
-// document.body.appendChild(product());
+document.body.innerHTML = root();
+const productsHolder = document.querySelector(".productsHolder");
+productsHolder.appendChild(await Product());
